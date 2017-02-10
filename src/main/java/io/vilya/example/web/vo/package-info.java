468ba9cbc93a3ecf0346da78047fae19e807647e
iamaprin <iamaprin@gmail.com>
2017-02-10 17:38:55 +0800

    #

---
+++ src/main/java/io/vilya/example/web/vo/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * spring-example
+ * io.vilya.example.web.vo.package-info
+ * 2017年2月10日 下午5:36:39
+ */
+/**
+ * @author zhukuanxin
+ * @time 2017年2月10日 下午5:36:39
+ */
+package io.vilya.example.web.vo;
